241f73a677857f3fc12ae1cbe318e82f180f124b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-18 00:40:10 -0700

    update

--- tensor_learn/introduction_tensor.py
+++ tensor_learn/introduction_tensor.py
@@ -8,3 +8,9 @@ tensor = torch.tensor(data)
 # initializing tensor directly from numpy array
 numpy_array = np.array(data)
 tensor_from_numpy = torch.tensor(numpy_array)
+
+# From another tensor (new tensor will have same shape and datatype)
+x_ones = torch.ones_like(tensor)
+
+# from another tensor with explicit datatype
+x_rand = torch.rand_like(tensor, dtype=torch.float)
