4e6b3dee704ed000ebf99bd3786eac34bb977d6d
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-19 06:55:10 -0700

    update

--- tensor_learn/dataset_and_dataloaders/chep.py
+++ tensor_learn/dataset_and_dataloaders/chep.py
@@ -1,22 +1,19 @@
 import tensorflow as tf
-
-# Check version
-print("TensorFlow version:", tf.__version__)
-
-# Check GPU availability
-print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
-
-# Build and run a small test model
 mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-predictions = model(x_train[:1]).numpy()
-print("Model output shape:", predictions.shape)
+model.compile(optimizer='adam',
+  loss='sparse_categorical_crossentropy',
+  metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
