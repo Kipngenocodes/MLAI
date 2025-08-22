48e475d9c4f8fa86352efb085011311dd69c0885
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-22 08:24:22 -0700

    update

--- tensor_learn/neural_networks/intro.py
+++ tensor_learn/neural_networks/intro.py
@@ -23,9 +23,34 @@ class NeuralNetwork(nn.Module):
 
     def forward(self, x):
         x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        logits = self.linear_relu_stack(x) 
         return logits
     
 # We create an instance of NeuralNetwork, and move it to the device, and print its structure.
 model = NeuralNetwork().to(device)
 print(model) 
+
+# Prediction Pobability 
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+# Model Layers
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+
+# Converting each 2D 28X28 image into a continous array of 784 Pixels using nn.Flatten
+flatten = nn.Flatten()
+input_image = flatten(input_image)
+print(input_image.size())
+# The linear is a module that applies a linear transformation on the input using its stored weights and biases.
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(input_image)   
+print(hidden1.size())
+
+#  nn.ReLU
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
