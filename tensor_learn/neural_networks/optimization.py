87d0f8477d2bb8ea23b9747dfb7885c486b958a0
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-25 01:35:44 -0700

    updates

--- tensor_learn/neural_networks/optimization.py
+++ tensor_learn/neural_networks/optimization.py
@@ -90,4 +90,12 @@ def test_loop(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")  
     
-    
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 100
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")   
