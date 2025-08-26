42e8ac8784cbbc0fe934ca9a29876a311e80f94a
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-26 05:45:28 -0700

    update

---
+++ TensorFlow/image_class.py
@@ -0,0 +1,8 @@
+import tensorflow as tf 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Checking the version
+print("TensorFlow version:", tf.__version__)
+
+# Importing the Fashion MNIST dataset
