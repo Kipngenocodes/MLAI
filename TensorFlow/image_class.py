f36ecdb0dc5a1ddbfeea362b0ded68be23393611
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-27 04:10:01 -0700

    update

--- TensorFlow/image_class.py
+++ TensorFlow/image_class.py
@@ -17,15 +17,15 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(4,4))
-for i in range(100):
-    plt.subplot(10,10,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(4,4))
+# for i in range(100):
+#     plt.subplot(10,10,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
 
 
 # Set up the Layers
@@ -53,3 +53,56 @@ print('\nTest accuracy:', test_acc)
 probability_model = tf.keras.Sequential([model, 
                                         tf.keras.layers.Softmax()])
 predictions = probability_model.predict(test_images)
+
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+    
+    
+# Veifying  Prediction
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+# using the trained model 
+# Grab an image from the test dataset.
+img = test_images[1]
+
+print(img.shape)
