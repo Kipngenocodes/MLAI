09bd1093b9b1f257d73b4ea24c895f4002f93e6e
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-27 03:41:13 -0700

    update

--- TensorFlow/image_class.py
+++ TensorFlow/image_class.py
@@ -12,8 +12,17 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 
 # Prepocess Data
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
 plt.show()
