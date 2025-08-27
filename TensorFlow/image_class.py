8eb01e4c56ff79a757fe804eca931787029d5ec0
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-27 03:39:15 -0700

    update

--- TensorFlow/image_class.py
+++ TensorFlow/image_class.py
@@ -2,7 +2,18 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Checking the version
-print("TensorFlow version:", tf.__version__)
 
 # Importing the Fashion MNIST dataset
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# Prepocess Data
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
