9336be68bb005b6c8728f6deb7eb067f09cde03b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-27 03:56:32 -0700

    update

--- TensorFlow/image_class.py
+++ TensorFlow/image_class.py
@@ -17,12 +17,39 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
+plt.figure(figsize=(4,4))
+for i in range(100):
+    plt.subplot(10,10,i+1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
+
+
+# Set up the Layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+# Compile the Model
+model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+
+# Train the Model
+model.fit(train_images, train_labels, epochs=10)
+
+# Evaluating the accuracy 
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+# Make predictions
+probability_model = tf.keras.Sequential([model, 
+                                        tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
