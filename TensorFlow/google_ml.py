199849a8e832bd0e4e2816b498fbca1a0303865e
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-31 08:54:39 -0700

    update on ml learning

--- TensorFlow/google_ml.py
+++ TensorFlow/google_ml.py
@@ -21,3 +21,27 @@ training_df = chicago_taxi_dataset.loc[:, ('TRIP_MILES', 'TRIP_SECONDS', 'FARE',
 print('Read dataset completed successfully.')
 print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
 training_df.head(200)
+
+# viewing Dataset Statistics
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.describe(include='all')
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print("What is the maximum fare? 				Answer: ${fare:.2f}".format(fare = max_fare))
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print("What is the mean distance across all trips? 		Answer: {mean:.4f} miles".format(mean = mean_distance))
+
+# How many cab companies are in the dataset?
+num_unique_companies =  training_df['COMPANY'].nunique()
+print("How many cab companies are in the dataset? 		Answer: {number}".format(number = num_unique_companies))
+
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print("What is the most frequent payment type? 		Answer: {type}".format(type = most_freq_payment_type))
+
+# Are any features missing data?
+missing_values = training_df.isnull().sum().sum()
+print("Are any features missing data? 				Answer:", "No" if missing_values == 0 else "Yes")
