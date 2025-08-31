4eb8d8ba8b7f5e608708607bacea434c7b4a4da0
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-08-31 09:04:23 -0700

    updates

--- TensorFlow/google_ml.py
+++ TensorFlow/google_ml.py
@@ -45,3 +45,78 @@ print("What is the most frequent payment type? 		Answer: {type}".format(type = m
 # Are any features missing data?
 missing_values = training_df.isnull().sum().sum()
 print("Are any features missing data? 				Answer:", "No" if missing_values == 0 else "Yes")
+
+
+# Checking on the correlation between features
+correlation_matrix = training_df.corr(numeric_only=True)
+
+# Visualizing the realationships in datasets
+px.scatter_matrix(training_df, dimensions=["FARE", "TRIP_MILES", "TRIP_SECONDS"])
+
+# Training a model
+def create_model(
+    settings: ml_edu.experiment.ExperimentSettings,
+    metrics: list[keras.metrics.Metric],
+) -> keras.Model:
+    """Create and compile a simple linear regression model."""
+    # Describe the topography of the model.
+    # The topography of a simple linear regression model
+    # is a single node in a single layer.
+    inputs = {name: keras.Input(shape=(1,), name=name) for name in settings.input_features}
+    concatenated_inputs = keras.layers.Concatenate()(list(inputs.values()))
+    outputs = keras.layers.Dense(units=1)(concatenated_inputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    # Compile the model topography into code that Keras can efficiently
+    # execute. Configure training to minimize the model's mean squared error.
+    model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=settings.learning_rate),
+                    loss="mean_squared_error",
+                    metrics=metrics)
+
+    return model
+
+
+def train_model(
+        experiment_name: str,
+        model: keras.Model,
+        dataset: pd.DataFrame,
+        label_name: str,
+        settings: ml_edu.experiment.ExperimentSettings,
+    ) -> ml_edu.experiment.Experiment:
+    """Train the model by feeding it data."""
+
+    # Feed the model the feature and the label.
+    # The model will train for the specified number of epochs.
+    features = {name: dataset[name].values for name in settings.input_features}
+    label = dataset[label_name].values
+    history = model.fit(x=features,
+                        y=label,
+                        batch_size=settings.batch_size,
+                        epochs=settings.number_epochs)
+
+    return ml_edu.experiment.Experiment(
+        name=experiment_name,
+        settings=settings,
+        model=model,
+        epochs=history.epoch,
+        metrics_history=pd.DataFrame(history.history),
+    )
+
+print("SUCCESS: defining linear regression functions complete.")
+
+# Training model with one feature 
+settings_1 = ml_edu.experiment.ExperimentSettings(
+    learning_rate = 0.001,
+    number_epochs = 20,
+    batch_size = 50,
+    input_features = ['TRIP_MILES']
+)
+
+metrics = [keras.metrics.RootMeanSquaredError(name='rmse')]
+
+model_1 = create_model(settings_1, metrics)
+
+experiment_1 = train_model('one_feature', model_1, training_df, 'FARE', settings_1)
+
+ml_edu.results.plot_experiment_metrics(experiment_1, ['rmse'])
+ml_edu.results.plot_model_predictions(experiment_1, training_df, 'FARE')
